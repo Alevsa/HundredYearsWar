987fb5eb799c53e928e4e6c08fe2f07d560d77e1
srowly <srowlands@hotmail.co.uk>
2014-01-10 21:17:15 +0000

    Pawn Resurrection
    Made it so you can resurrect the pawns, and the icon appears above them,
    all works for now.

--- Assets/Scripts/PawnResurrection.cs
+++ Assets/Scripts/PawnResurrection.cs
@@ -5,6 +5,12 @@ public class PawnResurrection : MonoBehaviour
 {
 	public float PawnHealth = 0f;
 
+	public float resurrectTime = 5f;
+
+	private bool resurrecting;
+	private float startTime;
+
+	private bool pawnAlive = false;
 	private bool bishopTop = false;
 	private bool bishopBottom = false;
 
@@ -12,8 +18,8 @@ public class PawnResurrection : MonoBehaviour
 	private Transform bishopCheckBottom;
 
 	private Transform resurrectLoc;
-	private GameObject resBar = null;
 	public GameObject ResurrectPrefab;
+	private GameObject cloneResurrect;
 
 	// Use this for initialization
 	void Start () 
@@ -27,21 +33,36 @@ public class PawnResurrection : MonoBehaviour
 	{
 		bishopTop = Physics2D.Linecast(transform.position, bishopCheckTop.position, 1 << LayerMask.NameToLayer ("Player"));
 		bishopBottom = Physics2D.Linecast(transform.position, bishopCheckBottom.position, 1 << LayerMask.NameToLayer ("Player"));
-	}
-
-	void FixedUpdate()
-	{
-	    if ((bishopTop || bishopBottom) && Input.GetButton ("Fire1") && PawnHealth == 0f) 
+	
+	    if ((bishopTop || bishopBottom) && Input.GetButton("Fire1") && !pawnAlive) 
 		{
-		Object cloneResurrect = Instantiate (ResurrectPrefab, resurrectLoc.position, resurrectLoc.rotation);
-		resBar = GameObject.Find ("resBar");	
-		Resurrect ();	
+		  resurrectTime -= Time.deltaTime;
+
+	      if(!resurrecting)
+			{
+			  resurrecting = true;
+		      cloneResurrect = (GameObject) Instantiate (ResurrectPrefab, resurrectLoc.position, resurrectLoc.rotation);
+			}
+
+			if(resurrectTime < 0)
+		       Resurrect ();
 	    }
+
+		if((!bishopTop && !bishopBottom) || !Input.GetButton("Fire1"))
+		{
+			resurrectTime = 5f;
+			resurrecting = false;
+			Destroy (cloneResurrect);
+		}
 
 	}
 
 	void Resurrect()
 	{
+		resurrectTime = 5f;
+		pawnAlive = true;
+		resurrecting = false;
+		Destroy (cloneResurrect);
 		PawnHealth = 100f;
 		transform.Rotate(new Vector3(0, 0, -90));
 	}
