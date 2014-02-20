480510cdc30e89ac2f0456aec8234fc9075734d0
srowly <srowlands@hotmail.co.uk>
2014-02-20 19:05:49 +0000

    pawn and frog behaviour

--- Assets/Scripts/FrogBehaviour.cs
+++ Assets/Scripts/FrogBehaviour.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class FrogBehaviour : MonoBehaviour 
 {
+	public float deathDuration = 60f;
+	
 	public bool facingRight = true;
 	public float moveForce = 100f;
 	public float maxSpeed = 2f;
@@ -12,14 +14,33 @@ public class FrogBehaviour : MonoBehaviour
 	private Transform checker;
 	private bool flipFrog = false;
 
+	public float health = 100f;
+	public float noDamagePeriod = 2f;
+	private float lastHitTime;
+	
+	private bool pawnClose = false;
+
+	private int oldLayer;
+	
+	private Animator anim;
+
+	private bool fell = false;
+
 	void Start () 
 	{
 		checker = transform.Find ("Checker");
+		anim = GetComponent<Animator>();
+
+		oldLayer = gameObject.layer;
+
 	}
 
 	void FixedUpdate () 
 	{
 		flipFrog = Physics2D.Linecast (transform.position, checker.position, 1<< LayerMask.NameToLayer ("Blocker"));
+		pawnClose = Physics2D.Linecast (transform.position, checker.position, 1<< LayerMask.NameToLayer ("Ally"));
+
+		anim.SetFloat("Speed", Mathf.Abs (h));
 
 		if (facingRight)
 			h = 1;
@@ -30,11 +51,46 @@ public class FrogBehaviour : MonoBehaviour
 		if (flipFrog) 
 			Flip();
 
+	    if (pawnClose) 
+		{
+			h = 0f;
+			Attack ();
+		}
+
+		if(!pawnClose)
+		{
+
 		if(rigidbody2D.velocity.x < maxSpeed)
 			rigidbody2D.AddForce(Vector2.right * h * moveForce);
 
 		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
 			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		}
+
+		if (health == 0) 
+		{
+			StartCoroutine(Death());
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if(col.gameObject.tag == "Pawn")
+		{
+			if(Time.time > lastHitTime + noDamagePeriod)
+			{
+			   if(health>20f)
+		       {
+				  lastHitTime = Time.time; 
+			      health -= 20f;
+			   }
+			   else
+			   {
+				  health -=20f;
+				  StartCoroutine(Death());
+			   }
+			}
+		}
 	}
 
 	void Flip ()
@@ -43,5 +99,33 @@ public class FrogBehaviour : MonoBehaviour
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+	}
+
+	void Attack()
+	{
+
+	}
+
+    IEnumerator Death()
+	{
+		h = 0f;
+		rigidbody2D.isKinematic = true;
+		this.gameObject.layer = 10;
+		if (!fell) 
+		{
+			this.transform.Rotate(0f, 0f, 90f);
+			this.transform.position += new Vector3(0f, -0.5f, 0f);
+			fell = true;
+		}
+		yield return new WaitForSeconds(deathDuration);
+		this.gameObject.layer = oldLayer;
+		rigidbody2D.isKinematic = false;
+		health = 100f;
+		if (fell) 
+		{
+			this.transform.Rotate(0f, 0f, -90f);
+			this.transform.position += new Vector3(0f, 0.5f, 0f);
+			fell = false;
+		}
 	}
 }
---
+++ Assets/Scripts/PawnAttack.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PawnAttack : MonoBehaviour 
+{
+	private bool FrogPresent = false;
+	private Transform FrogCheck;
+
+	private Animator anim;
+
+	private bool attack = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		FrogCheck = transform.Find ("frogCheck");
+		anim = GetComponent<Animator>();
+	}
+
+	void FixedUpdate () 
+	{
+		anim.SetBool("Attack", attack);
+		FrogPresent = Physics2D.Linecast (transform.position, FrogCheck.position, 1<< LayerMask.NameToLayer ("Ground"));
+
+		if (FrogPresent && (this.gameObject.layer == 11)) 
+			attack = true;
+		else
+			attack = false;
+
+	}
+}
